4f069c428194155a6626563140c9e5e6cc7c1d56
Adithya A <aadithyaajith2006@gmail.com>
2026-06-22 16:51:45 +0530

    Add files via upload

---
+++ stockportfolio output.py
@@ -0,0 +1,212 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+========================= RESTART: D:/stockportfolio.py ========================
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 1
+
+Enter Stock Symbol: AAPL
+Enter Quantity: 180
+Stock Added Successfully!
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 1
+
+Enter Stock Symbol: TSLA
+Enter Quantity: 250
+Stock Added Successfully!
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 1
+
+Enter Stock Symbol: GOOG
+Enter Quantity: 140
+Stock Added Successfully!
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 1
+
+Enter Stock Symbol: MSFT
+Enter Quantity: 330
+Stock Added Successfully!
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 1
+
+Enter Stock Symbol: AMZN
+Enter Quantity: 150
+Stock Added Successfully!
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 2
+
+YOUR PORTFOLIO
+------------------------------
+AAPL       180 shares
+TSLA       250 shares
+GOOG       140 shares
+MSFT       330 shares
+AMZN       150 shares
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 3
+
+Total Investment = $245900
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 4
+
+Current Value = $266350
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 5
+
+Profit = $20450
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 6
+
+Portfolio Saved Successfully!
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 7
+
+Portfolio Loaded Successfully!
+
+========================================
+STOCK PORTFOLIO TRACKER
+========================================
+1. Add Stock
+2. View Portfolio
+3. Investment Value
+4. Current Value
+5. Profit/Loss
+6. Save Portfolio
+7. Load Portfolio
+8. Exit
+
+Enter Choice: 8
+
+Thank You!
